92c820f853ffc38056859689ccab7e25f94279c5
PedroProbst <pedropb97@gmail.com>
2022-03-08 15:10:05 -0300

    Starting

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from board import Board
+from player import Player
+
+
+
+
+'''tabuleiro1 = Board()
+tabuleiro1.create_board1()
+print(tabuleiro1.matrix)
+
+tabuleiro2 = Board()
+tabuleiro2.create_board2()
+print(tabuleiro2.matrix)'''
+
+jogador1 = Player(position=[0,0])
+
+def main():
+    pygame.init
+
+
+
+main()
+
+
---
+++ player.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+
+
+class Player():
+    def __init__(self, position):
+        self.position = position
+
+    def mover_jogador():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.turn(CIMA)
+                elif event.key == pygame.K_DOWN:
+                    self.turn(BAIXO)
+                elif event.key == pygame.K_LEFT:
+                    self.turn(ESQUERDA)
+                elif event.key == pygame.K_RIGHT:
+                    self.turn(DIREITA)
+    
+
+
+
+#MOVIMENTOS
+CIMA = (0, -1)
+BAIXO = (0, 1)
+ESQUERDA = (-1, 0)
+DIREITA = (1, 0
+     
+    
