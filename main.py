6ee265cd4c8788660fd67599c8937ee3a2004e56
PedroProbst <pedropb97@gmail.com>
2022-03-11 16:46:32 -0300

    Adding funtions to main

--- main.py
+++ main.py
@@ -2,20 +2,41 @@ import pygame
 from board import Board
 from player import Player
 import sqlite3
+import sys
 
 #criando uma base de dados SQL para registrar as pontuações
 conn = sqlite3.connect('records.sqlite')
 cur = conn.cursor()
 cur.executescript('''
-DROP TABLE IF EXISTS Jogador;
-CREATE TABLE Jogador(
+CREATE TABLE IF NOT EXISTS Jogador(
     id        INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     nome      TEXT UNIQUE,
     score     INTEGER NOT NULL
 )''')
 
 name = input('Digite seu nome: ')
+cur.execute('''INSERT OR IGNORE INTO Jogador (nome) 
+        VALUES ( ? )''', ( name, ) )
+conn.commit()
 
+jogador1 = Player(position=[0,0])
+
+largura_tela = 800
+altura_tela = 600
+
+def main():
+    #chamando os métodos do pygame para iniciar o jogo
+    pygame.init
+    #dimensões da janela
+    pygame.display.set_mode((800,600))
+    #nome do programa aberto
+    pygame.display.set_caption('Thin Ice')
+    #definindo o clock em que o jogo roda
+    clock = pygame.time.Clock()
+    #definindo o tick em que o clock funciona(FPS)
+    clock.tick(60)
+
+    
 '''tabuleiro1 = Board()
 tabuleiro1.create_board1()
 print(tabuleiro1.matrix)'''
@@ -24,16 +45,7 @@ print(tabuleiro1.matrix)'''
 tabuleiro2.create_board2()
 print(tabuleiro2.matrix)'''
 
-jogador1 = Player(position=[0,0])
 
-def main():
-    pygame.init
-    
-    score = 0
-
-#executando o script SQL para inserir o nome e a pontuação na base de dados
-    cur.execute('''INSERT OR IGNORE INTO Jogador (nome)
-            VALUES ( ? )''', ( name, ) )
 
 
 
--- player.py
+++ player.py
@@ -5,22 +5,34 @@ import sys
 class Player():
     def __init__(self, position):
         self.position = position
+        self.size = 1
+
+    def turn(self, point):
+        self.direção = point
 
     def mover_jogador(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.turn(CIMA)
-                elif event.key == pygame.K_DOWN:
-                    self.turn(BAIXO)
-                elif event.key == pygame.K_LEFT:
-                    self.turn(ESQUERDA)
-                elif event.key == pygame.K_RIGHT:
-                    self.turn(DIREITA)
-    
+        #definindo o que faz o jogo parar (o jogador perde)
+        encurralado = False
+        while not encurralado:
+            #pygame.event.get() cria uma lista de eventos (qualquer clique ou movida de mouse por exemplo)
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT:
+                    encurralado = True
+                    print(event)
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.turn(CIMA)
+                    elif event.key == pygame.K_DOWN:
+                        self.turn(BAIXO)
+                    elif event.key == pygame.K_LEFT:
+                        self.turn(ESQUERDA)
+                    elif event.key == pygame.K_RIGHT:
+                        self.turn(DIREITA)
+            pygame.display.update()
+           
+        
 
 
 
